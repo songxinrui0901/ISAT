e62c1269e5aa1f031d014c5975a7240f3bc75b8d
songxinrui <1685362165@qq.com>
2025-01-15 20:47:24 +0800

    还不成熟的关于标签的分类，还没有与属性相连接

---
+++ Constant/KR_Unit_List.py
@@ -0,0 +1,166 @@
+#2024-01-14  1.0.0   First version
+# This file contains the list of units used in Kingdom Rush 4.
+# This list was made to differentiate between units used in Kingdom Rush 4。
+
+# Units used in Kingdom Rush 4
+KR_Unit_List = [
+    #Enermy Units
+    "Human Woodcutter",
+    "Human Worker",
+    "Dwarf Bruiser",
+    "Warhammer Guard",
+    "Clockwork Spider",
+    "Chomp Bot",
+    "Cyclopter",
+    "Tinbeard Gunman",
+    "Smokebeard Engineer",
+    "Sulfur Alchemist",
+    "Quarry Worker",
+    "Stonebeard Geomancer",
+    "MechaDwarf MK.9",
+    "Northern Wildling",
+    "Northern Huntress",
+    "Glacial Wolf",
+    "Bule Wyvern",
+    "Northern Berserker",
+    "Nanoq Warbear",
+    "Apex Stalker",
+    "Apex Shard",
+    "Ice Witch",
+    "Leap Dragon",
+    "Valkyrie",
+    "Draugr",
+    "Svell Druid",
+    "Frost Giant",
+    "Recruit",
+    "Footman",
+    "Watchdog",
+    "Troop Captain",
+    "Farmer_1",
+    "Farmer_2",
+    "Farmer_3",
+    "Farmer_4",
+    "Farmer_5",
+    "Hunting Eagle",
+    "Joe Jenkins",
+    "Baa San",
+    "Alleria Swiftwind",
+    "Elven Ranger",
+    "Devoted Priest",
+    "Gryphon Bombardier",
+    "Arcane Magus",
+    "High Sorcerer",
+    "Sheep",
+    "Paladin",
+    "Golem House",
+    "Shieldbearer",
+    "Cavalier",
+    "Musketeer",
+    "War Wagon",
+    # Enermy Abilities Units
+    "Chomp Bot_Broken",
+    "Smokebeard Engineer_Fixing",
+    "Quarry Worker_Digging",
+    "Stonebeard Geomancer_Frantic",
+    "MechaDwarf MK.9_Broken",
+    # Enermy Other Units
+    "Tinbeard Gunman_Bullet",
+    "Sulfur Alchemist_Chemical",
+    "Northern Huntress_Axe",
+    "Svell Druid_Ice",
+    "Elven Ranger_Arrow",
+    "Gryphon Bombardier_Bomb",
+    "Arcane Magus_energy",
+    "Musketeer_Bullet",
+    #Tower units
+    "Shadow Archers_Level1",
+    "Shadow Archers_Level2",
+    "Shadow Archers_Level3",
+    "Shadow Archers_Level4",
+    "Orc Warriors Den_Level1",
+    "Orc Warriors Den_Level2",
+    "Orc Warriors Den_Level3",
+    "Orc Warriors Den_Level4",
+    "Infernal Mage_Level1",
+    "Infernal Mage_Level2",
+    "Infernal Mage_Level3",
+    "Infernal Mage_Level4",
+    "Rocket Riders_Level1"
+    "Rocket Riders_Level2",
+    "Rocket Riders_Level3",
+    "Rocket Riders_Level4",
+    "Dark Knights_Level1",
+    "Dark Knights_Level2",
+    "Dark Knights_Level3",
+    "Dark Knights_Level4",
+    "Melting Furnace_Level1",
+    "Melting Furnace_Level2",
+    "Melting Furnace_Level3",
+    "Melting Furnace_Level4",
+    "Specters Mausoleum_Level1",
+    "Specters Mausoleum_Level2",
+    "Specters Mausoleum_Level3",
+    "Specters Mausoleum_Level4",
+    "Goblirangs_Level1",
+    "Goblirangs_Level2",
+    "Goblirangs_Level3",
+    "Goblirangs_Level4",
+    "Bone Flingers_Level1",
+    "Bone Flingers_Level2",
+    "Bone Flingers_Level3",
+    "Bone Flingers_Level4",
+    "Elite Harassers_Level1",
+    "Elite Harassers_Level2",
+    "Elite Harassers_Level3",
+    "Elite Harassers_Level4",
+    "Orc Shaman_Level1",
+    "Orc Shaman_Level2",
+    "Orc Shaman_Level3",
+    "Orc Shaman_Level4",
+    "Troll Mercenaries",
+    #Friend Units 主要是友方的单位，包括兵营的单位，以及因为技能而产生的单位
+    "Orc Warriors_Level1",
+    "Orc Warriors_Level2",
+    "Orc Warriors_Level3",
+    "Orc Warriors_Level4",
+    "Dark Knight_Level1",
+    "Dark Knight_Level2",
+    "Dark Knight_Level3",
+    "Dark Knight_Level4",
+    "Guardian Gargoyle",
+
+    #Tower Abilitues Other Units
+    "Shadow Archers_Arrow" #箭塔普通攻击弓箭
+    "Shadow Mark_Arrow", #箭塔暗影标记箭
+    "Blade of Demise_Kill", #箭塔秒杀
+    "Crow", #箭塔召唤兽——乌鸦
+    "Infernal Mage_Fire", #恶魔法师塔火焰普通攻击
+    "Infernal Mage_infernal rune", #恶魔法师塔恶魔符文技能
+    "Infernal Mage_lava geysers", #恶魔法师塔熔岩喷射技能
+    "Rocket Riders_Rocket_Level1", #火箭队塔普通攻击火箭等级1
+    "Rocket Riders_Rocket_Level2", #火箭队塔普通攻击火箭等级2
+    "Rocket Riders_Rocket_Level3", #火箭队塔普通攻击火箭等级3
+    "Rocket Riders_Rocket_Level4", #火箭队塔普通攻击火箭等级4
+    "Specter" #死灵墓普通攻击
+    
+    #Tower Abilities 这里仅仅记录了常用的技能的名字
+    "Shadow Mark",#箭塔技能一
+    "Blade of Demise", #箭塔技能二
+    "Crow's Nest", #箭塔技能三
+
+    "Affliction", #恶魔法师塔技能一
+    "Lava Fissure", #恶魔法师塔技能二
+    "Infernal Portal", #恶魔法师塔技能三
+
+    "Abrasive Heat", #熔炉技能二
+    "Burning Fuel"  #熔炉技能三
+    
+    "Guardian Gargoyles" #死灵墓技能一
+    "Possession" #死灵墓技能三
+
+
+    #Friend Abilities
+    #Heros Units
+    #Heros Abilities
+    #Heros Other Units
+    ]
